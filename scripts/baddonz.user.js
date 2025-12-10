f0c364021cffb22ab1cd89666ca95c849a179f07
ystygyc7q2-bit <ystygyc7q2@privaterelay.appleid.com>
2025-12-10 09:32:47 +0000

    Update baddonz.user.js

--- scripts/baddonz.user.js
+++ scripts/baddonz.user.js
@@ -10,9 +10,9 @@
 // @updateURL     https://besiak6.github.io/scripts/baddonz.user.js
 // ==/UserScript==
 (function() {
-    window.CSS_URL = "https://besiak6.github.io/styles/baddonz.css";
+    window.CSS_URL = "https://ystygyc7q2-bit.github.io/styles/baddonz.css";
     const version = Date.now();
-    const build = "https://besiak6.github.io/scripts/baddonz.js";
+    const build = "https://ystygyc7q2-bit.github.io/scripts/baddonz.js";
     const script = document.createElement("script");
     script.src = `${build}?v=${version}`;
     document.body.appendChild(script);
